e014170271f2a6650158afb236f7fadb305e260b
Rkknoob <iboonkhet@gmail.com>
2023-01-10 21:51:48 +0700

    update
    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
     <div class="container-fluid page-body-wrapper full-page-wrapper">
       <div class="content-wrapper d-flex align-items-center auth px-0 bocdy">
         <div class="row w-100 mx-0">
-          <div class="col-lg-4 mx-auto">
+          <div class="col-8 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5" style="border-radius: 30px;">
 
               {{-- <h4>Hello! let's get started</h4> --}}
