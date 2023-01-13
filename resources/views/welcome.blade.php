b0253aa69e5aa28414ecf5478ae3f3701a3db5e0
iboonkhet@gmail.com <iboonkhet@gmail.com>
2023-01-13 12:56:31 +0700

    update
    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
 
 <style>
     .bocdy  {
-      background-image: url("http://139.5.146.235/img/s11.png");
+      background-image: url("http://139.5.146.235/s11.png");
       background-size: 100% 100%;
 
 
