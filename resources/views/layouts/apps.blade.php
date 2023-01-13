aab49b9dbce59c182139d7835935014747fe93b2
iboonkhet@gmail.com <iboonkhet@gmail.com>
2023-01-13 12:18:29 +0700

    update
    update

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -30,7 +30,7 @@
     <style media="screen">
          .bocdy  {
     /* background-image: url("https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"); */
-      background-image: url("http://127.0.0.1:8000/Art.png");
+      background-image: url("http://139.5.146.235/Art.png");
       background-size: cover;
     background-repeat: no-repeat;
     background-position: center
