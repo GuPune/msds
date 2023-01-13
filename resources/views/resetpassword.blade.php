58a454af32f173c6209f473aa54bf965fdaed4e8
iboonkhet@gmail.com <iboonkhet@gmail.com>
2023-01-13 12:41:48 +0700

    update
    update

--- resources/views/resetpassword.blade.php
+++ resources/views/resetpassword.blade.php
@@ -44,7 +44,7 @@
                   <div class="form-group">
                     <label class="my-1 d-flex align-items-center" style="font-size: 20px;" for="role"><i class="fa fa-user-o" aria-hidden="true"></i>  E-MAIL <span class="ml-auto"></span></label>
 
-                    <input id="path" type="text" class="form-control form-control-lg" style="border-radius: 10px;"  name="path" value="" required >
+                    <input id="path" type="hidden" class="form-control form-control-lg" style="border-radius: 10px;"  name="path" value="" required >
                     <input id="email" type="email" class="form-control form-control-lg" style="border-radius: 10px;" placeholder="Email"  name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                     @error('email')
                     <span class="invalid-feedback" role="alert">
