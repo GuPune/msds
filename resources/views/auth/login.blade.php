1fc182998755fd6b02f1e5d5179f12df3243cc31
Rkknoob <iboonkhet@gmail.com>
2023-01-15 20:18:00 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,9 @@
 <style>
     .bocdy  {
         background-image: url("http://139.5.146.235/Art.png");
+        background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center
       /* background-image: url("https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"); */
 
     }
