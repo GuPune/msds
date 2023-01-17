468b6add41f848dd76ef03000b594fdc045a6ea7
iboonkhet@gmail.com <iboonkhet@gmail.com>
2023-01-17 10:49:51 +0700

    update
    update

--- resources/views/backend/editregis.blade.php
+++ resources/views/backend/editregis.blade.php
@@ -23,7 +23,7 @@
                 <div class="form-row">
                     <div class="form-group col-md-12">
                       <label for="inputEmail4">โทเคน</label>
-                      <input type="text" class="form-control" id="token" name="token" placeholder="Token" value="{{$sys->token}}" required>
+                      <input type="text" class="form-control" id="token" name="token" placeholder="Token" value="{{$sys->token}}" required disabled>
                     </div>
                   </div>
 
