0cd600c4baff001010e66b956999194d8b53a331
iboonkhet@gmail.com <iboonkhet@gmail.com>
2023-01-19 16:20:09 +0700

    update
    update

--- resources/views/pages/idol.blade.php
+++ resources/views/pages/idol.blade.php
@@ -304,7 +304,7 @@
             <div class="modal-header" style="justify-content: center;">
                 <img id="myImg" src="/aaaa.jpg" alt="Snow" style="width:50%;">
             </div>
-            <div class="modal-body">คุณ {{Auth::guard('web')->user()->fname}} ต้องการโหวตให้กับหมายเลข 03 ?</div>
+            <div class="modal-body">คุณ {{Auth::guard('web')->user()->fname}} ต้องการโหวตให้กับหมายเลข <span id="namemodal"></span>?</div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
                 <a class="btn btn-save-group" href="#">Votegroup</a>
