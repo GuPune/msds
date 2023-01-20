498793eca5773df07890375dc894693c79736ccf
iboonkhet@gmail.com <iboonkhet@gmail.com>
2023-01-20 13:41:36 +0700

    update
    update

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -26,8 +26,10 @@
     <!-- Font -->
     <link href="https://fonts.googleapis.com/css?family=Mitr&display=swap" rel="stylesheet">
 
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <style media="screen">
          .bocdy  {
     /* background-image: url("https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"); */
@@ -149,6 +151,7 @@ $(document).ready(function(){
     </div>-->
 
     <!-- Popper.JS -->
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <!-- Bootstrap JS -->
 
