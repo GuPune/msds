bdbcb016fb050740a4a0274c072fee2b6f9bbe32
Rkknoob <iboonkhet@gmail.com>
2023-01-30 20:07:28 +0700

    update
    update

--- resources/views/template/footer2.blade.php
+++ resources/views/template/footer2.blade.php
@@ -11,20 +11,20 @@ $ldate = date('Y-m-d H:i:s');
 @endphp
 @if($b)
 @if(($ldate > $b->start) && ($b->end > $ldate))
-    <div class="row" style="margin-bottom:2%; font-family:Mitr;">
-        {{-- <div class="col-4 text-center text-white" style="padding-top:1%;">
-          <a href="#"> <img src="/button-15389.png" alt="buttonpng" border="0" class="ima"/><br>HOME</a>
-        </div> --}}
-        <div class="col-4 text-center text-white" style="padding-top:1%;">
-          <a href="https://kahoot.it" target="_blank"><img src="/THE WALL.png" alt="buttonpng" border="0" class="ima"/></a>
-        </div>
-        <div class="col-4 text-center text-white" style="padding-top:1%;">
-          <a href="/dash/qafirst"><img src="/Q_A.png" alt="buttonpng" border="0" class="ima"/></a>
-        </div>
-        <div class="col-4 text-center text-white" style="padding-top:1%;">
-          <a href="/dash/votefirst"><img src="/VOTE.png" alt="buttonpng" border="0" class="ima"/></a>
-        </div>
-      </div>
+<div class="row" style="margin-bottom:2%; font-family:Mitr;">
+    {{-- <div class="col-4 text-center text-white" style="padding-top:1%;">
+      <a href="#"> <img src="/button-15389.png" alt="buttonpng" border="0" class="ima"/><br>HOME</a>
+    </div> --}}
+    <div class="col-4 text-center text-white" style="padding-top:1%;">
+
+    </div>
+    <div class="col-12 text-center text-white" style="padding-top:1%;">
+      <a href="/dash/home"><img src="/home.png" alt="buttonpng" border="0" class="ima2"/></a>
+    </div>
+    <div class="col-4 text-center text-white" style="padding-top:1%;">
+
+    </div>
+  </div>
 
 @endif
 
